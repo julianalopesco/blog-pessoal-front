bc4b011f204c7ff5e9f32f5b7fbbbe7cec966359
Juliana Lopes <118860220+julianalopesco@users.noreply.github.com>
2023-05-13 11:48:07 +0000

    criação grids no react

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -3,27 +3,31 @@ import './Home.css';
 import {Typography } from '@material-ui/core';
 import Box from '@mui/material/Box';
 import AddBoxIcon from '@mui/icons-material/AddBox';
-import {Grid} from '@mui/material'
+import {Grid, Paper} from '@material-ui/core';
+
 
 function Home() {
     return (
         // a função retorna apenas um elemento, portanto usa-se as divs/containers
         //a box é uma variavel que funciona como div, é uma div personalizável
-        // o grid aplia responsividade, através de colunas. xs > extra small, md > medium
-        <Box
-        display='flex'
-        justifyContent='center'
-      >
-        <div>
-            <Grid xs={10} md={6}>
-          <Typography align='center' variant='h4'>Clube do livro</Typography>
-          <h1 className='texto'>Clube do Livro</h1>
-          <AddBoxIcon/>
-          <img className="imgHome" src="src/assets/estante.jpg" alt="estante"/>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero facere id voluptatum unde a. Hic nam excepturi, voluptate odit placeat minima deleniti eligendi dicta optio ipsa unde doloribus omnis sequi alias neque, vel beatae magnam nihil! In ipsum quasi doloremque dolore sint quod quos animi, tenetur porro! Natus, earum saepe.</p>
+        // o grid aplica responsividade, através de colunas. xs > extra small, md > medium
+       <div>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={8}>
+            <Paper style={{ height: "100vh", background: "lightgrey" }}/>
           </Grid>
-          </div>
-      </Box>
+
+          <Grid item container direction="column" xs={12} sm={4} spacing={2}>
+            <Grid item>
+              <Paper style={{ height: "49vh", background: "orange" }} />
+            </Grid>
+            <Grid item>
+              <Paper style={{ height: "49vh", background: "green" }} />
+            </Grid>
+
+          </Grid>
+        </Grid>
+       </div>
     )
 }
 
