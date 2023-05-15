74dda0b927b7f6b77c7367b48512265c18d63d63
Juliana Lopes <118860220+julianalopesco@users.noreply.github.com>
2023-05-15 18:02:53 +0000

    criação componentes navbar e footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { useState } from 'react'
+import {Grid} from '@material-ui/core'; 
 import Home from './paginas/home/Home';
+import Navbar from './components/estaticos/navbar/Navbar'
+import Footer from './components/estaticos/footer/Footer'
 import './App.css'
 
+//<>: fragment, substitui o <div> 
+// o grid aplica responsividade, através de colunas. xs > extra small, md > medium
 function App() {
     return (
-    <Home />
+        <> 
+            <Navbar />
+            <Home />
+            <Footer />
+        </>
     )
 }
 
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,32 +1,34 @@
 import React from 'react';
 import './Home.css';
-import {Typography } from '@material-ui/core';
 import Box from '@mui/material/Box';
 import AddBoxIcon from '@mui/icons-material/AddBox';
-import {Grid, Paper} from '@material-ui/core';
+import {Grid, Paper, Button, Typography} from '@material-ui/core';
 
 
 function Home() {
     return (
         // a função retorna apenas um elemento, portanto usa-se as divs/containers
         //a box é uma variavel que funciona como div, é uma div personalizável
-        // o grid aplica responsividade, através de colunas. xs > extra small, md > medium
+        //p= padding
        <div>
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={8}>
-            <Paper style={{ height: "100vh", background: "lightgrey" }}/>
-          </Grid>
-
-          <Grid item container direction="column" xs={12} sm={4} spacing={2}>
-            <Grid item>
-              <Paper style={{ height: "49vh", background: "orange" }} />
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src ="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
             </Grid>
-            <Grid item>
-              <Paper style={{ height: "49vh", background: "green" }} />
-            </Grid>
-
-          </Grid>
-        </Grid>
        </div>
     )
 }
